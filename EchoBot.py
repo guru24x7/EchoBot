82ae1ab6c3e0003e22020d65b340110407176ecf
gaming guru <gaming.guru24x7@gmail.com>
2016-09-08 14:32:05 +0500

    Initial commit

---
+++ EchoBot.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+
+import sys
+import logging
+import getpass
+from optparse import OptionParser
+
+import sleekxmpp
+
+if sys.version_info < (3, 0):
+    from sleekxmpp.util.misc_ops import setdefaultencoding
+    setdefaultencoding('utf8')
+else:
+    raw_input = input
+
+
+class EchoBot(sleekxmpp.ClientXMPP):
+    def __init__(self, jid, password):
+        super(EchoBot, self).__init__(jid, password)
+
+        self.add_event_handler('session_start', self.start)
+        self.add_event_handler('message', self.message)
+
+    def start(self, event):
+        self.send_presence()
+        self.get_roster()
+
+    def message(self, msg):
+        if msg['type'] in ('normal','chat'):
+            msg.reply("Thanks for sending\n%(body)s" % msg).send()
+
+if __name__ == '__main__':
+    optp = OptionParser()
+
+    optp.add_option('-d', '--debug', help='set logging to DEBUG',
+                    action='store_const', dest='loglevel',
+                    const=logging.DEBUG, default=logging.INFO)
+    optp.add_option("-j", "--jid", dest="jid",
+                    help="JID to use")
+    optp.add_option("-p", "--password", dest="password",
+                    help="password to use")
+
+    opts, args = optp.parse_args()
+
+    logging.basicConfig(level=opts.loglevel,
+                        format='%(levelname)-8s %(message)s')
+
+    if opts.jid is None:
+        opts.jid = raw_input("Username: ")
+    if opts.password is None:
+        opts.password = getpass.getpass("Password: ")
+
+    xmpp = EchoBot(opts.jid, opts.password)
+    xmpp.register_plugin('xep_0030') # Service Discovery
+    xmpp.register_plugin('xep_0004') # Data Forms
+    xmpp.register_plugin('xep_0060') # PubSub
+    xmpp.register_plugin('xep_0199') # XMPP Ping
+
+    if xmpp.connect(('192.168.126.128', 5222)):
+        xmpp.process(block=True)
+    else:
+        print('Unable to connect')
